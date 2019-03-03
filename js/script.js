f9b851c39bd7e2f980914cf774a60cbc462d2ea5
kiverchuk <32567219+kiverchuk@users.noreply.github.com>
2019-03-03 23:20:16 +0200

    crearea variabilelor butoanelor pentru colnrol in js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,47 @@
 var json = null;
 window.onload = function(){
-	let comand = document.querySelector('.comand');
+	//comand buttons
+	let comandbtn = document.querySelector('.comand');
 	let closecomand = document.querySelector('.btnclose');
 	let comandpanel = document.querySelector('.absolute');
+	let coshopen = document.querySelector('.coshopen');
+	let closecosh = document.querySelector('.coshclose');
+	let coshpanel = document.querySelector('.coshpanel');
+
+	//comfort buttons
+	let comfort1 = document.querySelector('#comfort1');
+	let comfort2 = document.querySelector('#comfort2');
+	let comfort3 = document.querySelector('#comfort3');
+	let comfort4 = document.querySelector('#comfort4');
+	let comfort5 = document.querySelector('#comfort5');
+	let comfort6 = document.querySelector('#comfort6');
+	let comfort7 = document.querySelector('#comfort7');
+	let comfort8 = document.querySelector('#comfort8');
+
+	//securitatea buttons
+	let secure1 = document.querySelector('#secure1');
+	let secure2 = document.querySelector('#secure2');
+	let secure3 = document.querySelector('#secure3');
+	let secure4 = document.querySelector('#secure4');
+	let secure5 = document.querySelector('#secure5');
+	let secure6 = document.querySelector('#secure6');
+	let secure7 = document.querySelector('#secure7');
+	let secure8 = document.querySelector('#secure8');
+
+	//cras sets
+	let color = document.querySelector('#color');
+	let colorin = document.querySelector('#colorin');
+	let discuri = document.querySelector('#discuri');
+	let audion = document.querySelector('#audio');
+	let aer = document.querySelector('#aer');
+	let parbrize = document.querySelector('#parbrize');
 
 	(async () => {
 	  const response = await fetch('./js/data.json')
 	  json = await response.json()
 	})()
 
-	let basicServicesSelect = document.querySelector('#lkjh');
+	
 		basicServicesSelect.onchange = function(){
 		console.log(basicServicesSelect.checked);
 	}
@@ -18,7 +50,7 @@ window.onload = function(){
 	// }, 1000);
 
 	
-	comand.addEventListener("click", function(){
+	comandbtn.addEventListener("click", function(){
 		comandpanel.style.display = 'block';
 	});
 	closecomand.addEventListener("click", function(){
